41db4343ad360ad94a7ad7a94c0830f3c7e51c9b
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-11-02 20:24:55 +0200

    11-binary_tree_size.c

--- binary_trees.h
+++ binary_trees.h
@@ -46,6 +46,7 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 
 size_t binary_tree_height(const binary_tree_t *tree);
 size_t binary_tree_depth(const binary_tree_t *tree);
+size_t binary_tree_size(const binary_tree_t *tree);
 
 int binary_tree_is_perfect(const binary_tree_t *tree);
 size_t binary_tree_height_16(const binary_tree_t *tree);
